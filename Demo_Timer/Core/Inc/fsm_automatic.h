a56bf2116e27fa5e363f74c816421cc1966cbe1e
Vo Duc Minh <92356136+minhVoDuc@users.noreply.github.com>
2022-10-04 07:39:12 +0700

    create stm32 project

---
+++ Demo_Timer/Core/Inc/fsm_automatic.h
@@ -0,0 +1,16 @@
+/*
+ * fsm_automatic.h
+ *
+ *  Created on: 4 thg 10, 2022
+ *      Author: Guest_demo
+ */
+
+#ifndef INC_FSM_AUTOMATIC_H_
+#define INC_FSM_AUTOMATIC_H_
+
+#include "global.h"
+#include "software_timer.h"
+
+void fsm_automatic_run();
+
+#endif /* INC_FSM_AUTOMATIC_H_ */
---
+++ Demo_Timer/Core/Src/fsm_automatic.c
@@ -0,0 +1,20 @@
+/*
+ * fsm_automatic.c
+ *
+ *  Created on: 4 thg 10, 2022
+ *      Author: Guest_demo
+ */
+
+#include "fsm_automatic.h"
+
+void fsm_automatic_run(){
+	switch (status){
+		case 0:
+				break;
+
+		case 1:
+				break;
+
+		default: break;
+	}
+}
