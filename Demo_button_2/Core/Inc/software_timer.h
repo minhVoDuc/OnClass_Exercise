8a52de0280f9d4bcaf30ecc454b81dff332295c1
Vo Duc Minh <92356136+minhVoDuc@users.noreply.github.com>
2022-10-04 09:45:15 +0700

    add to exercise repo

---
+++ Demo_button_2/Core/Inc/software_timer.h
@@ -0,0 +1,14 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Sep 27, 2022
+ *      Author: Guest_demo
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+void setTimer(int duration);
+void runTimer();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
---
+++ Demo_button_2/Core/Src/software_timer.c
@@ -0,0 +1,9 @@
+/*
+ * software_timer.c
+ *
+ *  Created on: Sep 27, 2022
+ *      Author: Guest_demo
+ */
+
+#include "software_timer.h"
+
