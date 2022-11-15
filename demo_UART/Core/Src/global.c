6275da8861a279baf2a55eab99f1508a7cdcc131
Vo Duc Minh <92356136+minhVoDuc@users.noreply.github.com>
2022-11-15 07:18:19 +0700

    create stm32

---
+++ demo_UART/Core/Src/global.c
@@ -0,0 +1,11 @@
+/*
+ * global.c
+ *
+ *  Created on: Nov 15, 2022
+ *      Author: Guest_demo
+ */
+
+#include "global.h"
+
+int Error_code_G = 0
+
---
+++ demo_UART/Core/Src/software_timer.c
@@ -0,0 +1,36 @@
+/*
+ * software_timer.c
+ *
+ *  Created on: Nov 15, 2022
+ *      Author: Guest_demo
+ */
+
+#include "software_timer.h"
+
+int TimerCounter[MAX_TIMER] = {0};
+int TimerFlag[MAX_TIMER] = {0};
+
+void set_timer(int index, int duration){
+	TimerCounter[index] = duration / CYCLE;
+	TimerFlag[index] = 0;
+}
+
+int check_timer_flag(int index){
+	if (TimerFlag[index] == 1){
+		TimerFlag[index] = 0;
+		return 1;
+	}
+	return 0;
+}
+
+void run_timer(){
+	for (int i=0; i < MAX_TIMER; i++){
+		if (TimerCounter[i] >= 0){
+			TimerCounter[i]--;
+			if (TimerCounter[i] <= 0){
+				TimerFlag[i] = 1;
+			}
+		}
+	}
+}
+
